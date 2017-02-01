9ff97ca721413a31702ea035f1f06dbc9d69c3c7
Patrick Fischer <fischerpl18@gmail.com>
2017-02-01 14:52:20 -0600

    Initial commit

---
+++ app/src/main/java/lipscomb/bisonswaptest/app/AppConfig.java
@@ -0,0 +1,13 @@
+package lipscomb.bisonswaptest.app;
+
+/**
+ * Created by fischer on 2/1/17.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://138.197.12.81/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://138.197.12.81/android_login_api/register.php";
+}
